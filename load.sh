0a43944323eabe6b29e3c058e4adaa77c9fad8cf
Jeff Squyres <jsquyres@cisco.com>
2013-07-21 06:16:14 -0400

    Update for where xcode 4.6.3 compiled it

--- load.sh
+++ load.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-cp -R ./build/Release/osx-pl2303.kext /System/Library/Extensions
+set -x
+
+dir=$HOME/Library/Developer/Xcode/DerivedData/osx-pl2303-bzejcgocbpnvvlbbeagtuborqkog/Build/Products/Debug
+cp -R $dir/osx-pl2303.kext /System/Library/Extensions
 cd /System/Library/Extensions
 chown -R root:wheel osx-pl2303.kext
 chmod -R 755 osx-pl2303.kext
