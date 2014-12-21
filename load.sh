9055c098d329860b24d6ca018c76402181627e7c
Jeff Squyres <jsquyres@cisco.com>
2014-12-21 12:27:31 -0500

    load.sh: make it mo' smarter
    Make it able to guess where the directory is, or take a $1 if the user
    knows where it is.

--- load.sh
+++ load.sh
@@ -1,7 +1,43 @@
 #!/bin/sh
+
+#
+# Take a good guess at where XCode built the thing.
+# Or, if $1 is supplied, try to use that
+#
+
+if test -n "$1"; then
+    dir=$1
+else
+    dir=$HOME/Library/Developer/Xcode/DerivedData
+    subdir=`/bin/ls -d $dir/osx-pl2303-*`
+    if test -z "$subdir"; then
+        echo "==== ERROR: Could not find right subdirectory"
+        echo "==== $dir"
+        exit 1
+    fi
+    if test "`echo $subdir | wc -w`" -ne 1; then
+        echo "==== ERROR: Found more than one possible subdirectory"
+        echo "==== $dir"
+        exit 1
+    fi
+    # For XCode 6.1.1
+    dir="$subdir/Build/Products/Debug"
+fi
+
+if test ! -d $dir; then
+    echo "==== ERROR: directory does not exist"
+    echo "==== $dir"
+    exit 1
+fi
+
+if test $EUID -ne 0; then
+    echo "==== ERROR: must run this script as root"
+    exit 1
+fi
+
+echo "==== Loading the kernel driver..."
 set -x
 
-dir=$HOME/Library/Developer/Xcode/DerivedData/osx-pl2303-bzejcgocbpnvvlbbeagtuborqkog/Build/Products/Debug
 cp -R $dir/osx-pl2303.kext /System/Library/Extensions
 cd /System/Library/Extensions
 chown -R root:wheel osx-pl2303.kext
